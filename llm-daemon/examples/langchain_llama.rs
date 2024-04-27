d4d2a57abcd100a6ea87243b0db602eccdc14362
Jeong, Hun <blmarket@gmail.com>
2024-04-27 11:59:53 -0700

    Example working with more cases

--- llm-daemon/examples/langchain_llama.rs
+++ llm-daemon/examples/langchain_llama.rs
@@ -1,4 +1,5 @@
 use llm_daemon::{LlamaConfig, LlamaDaemon, LlmDaemon};
+use langchain_rust::{language_models::{llm::LLM, options::CallOptions}, llm::{OpenAI, OpenAIConfig}, schemas::Message};
 
 fn main() -> anyhow::Result<()> {
     let daemon = LlamaDaemon::new(LlamaConfig::default());
@@ -6,7 +7,25 @@ fn main() -> anyhow::Result<()> {
     let runtime = tokio::runtime::Runtime::new()?;
     runtime.spawn(daemon.heartbeat());
     runtime.block_on(async {
-        println!("Hello world");
+        // FIXME: Use endpoint provided by daemon
+        // daemon needs startup time
+        let oai: OpenAI<OpenAIConfig> = OpenAI::new(OpenAIConfig::new().with_api_base(
+            "http://127.0.0.1:28282/v1".to_string()));
+        let msg0 = Message::new_human_message("Hello, how are you?");
+        let resp1 = oai.generate(&vec![msg0]).await?;
+        dbg!(resp1);
+        
+        let msg1 = Message::new_human_message("What is the sum of 7 and 8?");
+        let msg2 = Message::new_ai_message("The sum of 7 and 8 is ");
+        let mut oai2 = oai.clone();
+        oai2.add_options(CallOptions {
+            max_tokens: Some(1),
+            stop_words: Some(vec![".".to_string()]),
+            ..Default::default()
+        });
+        let resp2 = oai2.generate(&vec![msg1, msg2]).await?;
+        dbg!(resp2);
+
         Ok(())
     })
 }
