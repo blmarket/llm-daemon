b812a8fc3bbca0d1cf5b0a94cb9a564fb6ab6648
Jeong, Hun <blmarket@gmail.com>
2024-04-28 08:17:04 -0700

    Remove adhoc develop part from example script
    Use adhoc.py if you need to develop with local build.

--- bihyung/langchain_example.py
+++ bihyung/langchain_example.py
@@ -5,14 +5,6 @@ while not os.path.exists(".git"):
 os.chdir("bihyung")
 os.getcwd()
 
-import sys
-
-module_path = os.path.abspath(os.path.join('./bihyung'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-import maturin_import_hook
-maturin_import_hook.install()
 from bihyung import LlamaDaemon
 
 #%%
