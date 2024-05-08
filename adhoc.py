b6ff185bdc31a206d9b1a011638527644150089e
Jeong, Hun <blmarket@gmail.com>
2024-05-07 20:33:23 -0700

    Demo script update to show...
    how to use the context protocol
    using OpenAI client

--- adhoc.py
+++ adhoc.py
@@ -1,13 +1,10 @@
 #%%
+# First cell is for the development.
 import os
 while not os.path.exists(".git"):
     os.chdir("..")
 os.chdir("bihyung")
 os.getcwd()
-
-#%%
-# Temporary: I'm using custom build maturin, so can't use import_hook
-from bihyung import Model, daemon_from_model, server_path
 
 #%%
 import sys
@@ -18,30 +15,43 @@ if module_path not in sys.path:
 
 import maturin_import_hook
 maturin_import_hook.install()
-from bihyung import Model, daemon_from_model, server_path
-
-#%%
-d = daemon_from_model(Model.Gemma2b, server_path)
-
-#%%
-d.fork_daemon()
-d.heartbeat()
-
-#%%
-d.endpoint()
 
 #%%
 import requests
+from bihyung import Model, daemon_from_model
 
 #%%
-resp = requests.post(d.endpoint() + "/completions", json = {
+with daemon_from_model(Model.Gemma2b) as inner:
+    print(inner)
+    
+#%%
+inner = daemon_from_model(Model.Gemma2b)
+inner.__enter__()
+
+#%%
+resp = requests.post(inner.endpoint() + "/completions", json = {
     "prompt": "<bos>Hello world",
     "n_predict": 128,
     "max_tokens": 128,
 }).json()
 resp["content"]
 
+#%%
+# Section: OpenAI demo
+from openai import OpenAI
+
 # %%
-resp
+client = OpenAI(base_url = inner.endpoint())
+resp = client.completions.create(model = "base", prompt = "<bos>Hello world", max_tokens = 128)
+resp.content
+
+# %%
+resp = client.chat.completions.create(model = "base", messages = [{"role": "user", "content": "Hello world"}])
+resp.choices[0].message.content
+
+#%%
+# After __exit__, we no longer use the daemon.
+# Daemon will be kept alive for next 120 seconds, then close itself.
+inner.__exit__()
 
 # %%
