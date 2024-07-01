2dab633326f6c833554f25a67b8ceae6d63ca775
Jeong, Hun <blmarket@gmail.com>
2024-06-30 18:56:35 -0700

    Updated llama.cpp

--- llama-cpp-low/build.rs
+++ llama-cpp-low/build.rs
@@ -17,7 +17,7 @@ fn main() {
 
     if env::var("CARGO_CFG_TARGET_OS").unwrap() != "macos" {
         if cuda == "1" {
-            cmake.configure_arg("-DLLAMA_CUDA=ON");
+            cmake.configure_arg("-DGGML_CUDA=ON");
         }
     }
 
@@ -26,16 +26,17 @@ fn main() {
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    std::fs::copy(
-        submodule_dir.join("ggml-metal.metal"),
-        out_path.join("../../../ggml-metal.metal"),
-    )
-    .expect("Couldn't copy ggml-metal.metal");
-    std::fs::copy(
-        submodule_dir.join("ggml-common.h"),
-        out_path.join("../../../ggml-common.h"),
-    )
-    .expect("Couldn't copy ggml-metal.metal");
+    // Need to test with METAL environment
+    // std::fs::copy(
+    //     submodule_dir.join("src/ggml/ggml-metal.metal"),
+    //     out_path.join("../../../ggml-metal.metal"),
+    // )
+    // .expect("Couldn't copy ggml-metal.metal");
+    // std::fs::copy(
+    //     submodule_dir.join("ggml-common.h"),
+    //     out_path.join("../../../ggml-common.h"),
+    // )
+    // .expect("Couldn't copy ggml-common.h");
     std::fs::copy(
         dst.join("build/bin/llama-server"),
         out_path.join("../../../server"),
