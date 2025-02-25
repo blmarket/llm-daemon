95580dce2e6bd5e663f85a43ca031f28b08b1830
Jeong, Hun <blmarket@gmail.com>
2025-02-24 19:02:33 -0800

    More detailed feature list

--- llm-daemon/src/lib.rs
+++ llm-daemon/src/lib.rs
@@ -1,6 +1,7 @@
 mod daemon_trait;
 #[cfg(feature = "llama-daemon")]
 mod llama_daemon;
+#[cfg(feature = "proxy")]
 mod proxy;
 mod test_client;
 mod util;
@@ -11,5 +12,6 @@ pub use llama_daemon::{
     daemon_ext, llama_config_map, Daemon as LlamaDaemon, Daemon2, LlamaConfig,
     LlamaConfigs, Llamafile, LlamafileConfig,
 };
+#[cfg(feature = "proxy")]
 pub use proxy::{Proxy, ProxyConfig};
 pub use test_client::Generator;
