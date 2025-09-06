1f18e96e308c71b56d6feb8281606ed13507ba75
Jeong, Hun <blmarket@gmail.com>
2025-09-05 18:38:01 -0700

    Use updated parameters

--- node-llm-daemon/src/lib.rs
+++ node-llm-daemon/src/lib.rs
@@ -1,10 +1,16 @@
 #![deny(clippy::all)]
 
-use llm_daemon::{Daemon3, LlmDaemon};
+use llm_daemon::{Daemon3, Daemon3Params, LlmDaemon};
 use napi_derive::napi;
+use std::path::PathBuf;
 
 #[napi]
 pub fn spawn_daemon(hf_repo: String, args: Vec<String>) {
-  let daemon = Daemon3::new(hf_repo, args);
+  let daemon = Daemon3::new(Daemon3Params {
+    hf_repo,
+    args: Some(args),
+    port: None,
+    server_binary: Some(PathBuf::from("server.linux-x64-gnu")),
+  });
   let _ = daemon.fork_daemon();
 }
