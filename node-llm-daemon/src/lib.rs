882d7223bdf7037b4b759a38385721e141dc0a9a
Jeong, Hun <blmarket@gmail.com>
2025-09-05 18:45:31 -0700

    Trying to make reasonable path for server binary

--- node-llm-daemon/src/lib.rs
+++ node-llm-daemon/src/lib.rs
@@ -2,15 +2,14 @@
 
 use llm_daemon::{Daemon3, Daemon3Params, LlmDaemon};
 use napi_derive::napi;
-use std::path::PathBuf;
 
 #[napi]
 pub fn spawn_daemon(hf_repo: String, args: Vec<String>) {
-  let daemon = Daemon3::new(Daemon3Params {
+  let daemon = Daemon3::from(Daemon3Params {
     hf_repo,
     args: Some(args),
     port: None,
-    server_binary: Some(PathBuf::from("server.linux-x64-gnu")),
+    server_binary: Some(std::env::current_dir().unwrap().join("artifacts/server")),
   });
   let _ = daemon.fork_daemon();
 }
